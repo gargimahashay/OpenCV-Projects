1bef2ede36d799a6c7c8b695eb169377aafa0e3a
Gargi Mahashay <108257736+gargimahashay@users.noreply.github.com>
2022-11-07 12:21:17 +0530

    Add files via upload

---
+++ setting_camera_parameter/setting_camera_parameter.py
@@ -0,0 +1,21 @@
+import cv2
+cap = cv2.VideoCapture(0)
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+cap.set(3, 1208)
+cap.set(4, 720)
+
+while(cap.isOpened()):
+    ret, frame = cap.read()q
+    if ret == True:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', gray)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
